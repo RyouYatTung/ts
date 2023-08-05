973527779f4fca89074df96f76762575172be3cb
RyouYatTung <leungyattung1108@gmail.com>
2023-08-05 21:42:07 +0900

    push

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,1 +1,10 @@
-console.log("hi");
+"use strict";
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+        this.collection = collection;
+    }
+    sort() { }
+}
+const sorter = new Sorter([20, 3, 5, 6, -4]);
+sorter.sort();
